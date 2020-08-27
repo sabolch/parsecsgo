c8011a3b0716fba024960e7f1e8edf92366428c9
Leonardus Pisanus <sabolch@yandex.ru>
2020-08-27 17:57:49 +0300

    ///

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ import datetime
 bot = telebot.TeleBot('1089262513:AAEKgwH1drH3vymrnaN79h1jM8JEQ4HRpK0')
 player_link = ''
 player_list = []
-global date_message
 # d_user = input("date")
 
 d1 = date(2019, 1, 1)
@@ -16,70 +15,99 @@ d2 = date.today()
 print(d2.strftime('%Y.%m.%d'))
 
 
-# функция построения диапазона дат от d1 до d2
-def date_range(d1, d2):
-    return (d1 + datetime.timedelta(days=i) for i in range((d2 - d1).days + 1))
-
-
-# вывод всех
-for d in date_range(d1, d2):
-    print(d.strftime('%Y.%m.%d'))
-
-
-# функция календаря, принимает мессендж
-def start(m):
-    calendar, step = DetailedTelegramCalendar().build()
-    bot.send_message(m.chat.id,
-                     f"Select {LSTEP[step]}",
-                     reply_markup=calendar)
-
-
-# Настройки календаря
-@bot.callback_query_handler(func=DetailedTelegramCalendar.func())
-def cal(c):
-    result, key, step = DetailedTelegramCalendar(locale='ru', min_date=d1, max_date=d2).process(c.data)
-    if not result and key:
-        bot.edit_message_text(f"Select {LSTEP[step]}",
-                              c.message.chat.id,
-                              c.message.message_id,
-                              reply_markup=key)
-    elif result:
-        bot.edit_message_text(f"Min: {result.strftime('%d.%m.%Y')}",
-                              c.message.chat.id,
-                              c.message.message_id)
-
-        print(f"qqqq {c.message.message_id}")
-
-print()
 # Декоратор для обработки всех текстовых сообщений
 @bot.message_handler(content_types=['text'])
 def all_messages(message):
+    # функция календаря, принимает мессендж
+    def start(m):
+        calendar, step = DetailedTelegramCalendar().build()
+        bot.send_message(m.chat.id,
+                         f"Select {LSTEP[step]}",
+                         reply_markup=calendar)
+
+
+
+
+
+    # Настройки календаря
+    @bot.callback_query_handler(func=DetailedTelegramCalendar.func())
+    def cal(c):
+        result, key, step = DetailedTelegramCalendar(locale='ru', min_date=d1, max_date=d2).process(c.data)
+        if not result and key:
+            bot.edit_message_text(f"Select {LSTEP[step]}",
+                                  c.message.chat.id,
+                                  c.message.message_id,
+                                  reply_markup=key)
+        elif result:
+            bot.edit_message_text(f"Min: {result.strftime('%d.%m.%Y')}",
+                                  c.message.chat.id,
+                                  c.message.message_id)
+
+            # функция построения диапазона дат от d1 до d2
+            def date_range(d1, d2):
+                return (d1 + datetime.timedelta(days=i) for i in range((d2 - d1).days + 1))
+
+            first_date = result
+            # вывод всех
+            for d in date_range(first_date, d2):
+                print(d.strftime('%Y.%m.%d'))
+
+
+            def start2(m):
+                calendar, step = DetailedTelegramCalendar().build()
+                bot.send_message(m.chat.id,
+                                 f"Select {LSTEP[step]}",
+                                 reply_markup=calendar)
+
+            start2(message)
+
+            def cal2(c):
+                result, key, step = DetailedTelegramCalendar(locale='ru', min_date=d1, max_date=d2).process(c.data)
+                if not result and key:
+                    bot.edit_message_text(f"Select {LSTEP[step]}",
+                                          c.message.chat.id,
+                                          c.message.message_id,
+                                          reply_markup=key)
+                elif result:
+                    bot.edit_message_text(f"Min: {result.strftime('%d.%m.%Y')}",
+                                          c.message.chat.id,
+                                          c.message.message_id)
+
+                    # функция построения диапазона дат от d1 до d2
+                    def date_range(d1, d2):
+                        return (d1 + datetime.timedelta(days=i) for i in range((d2 - d1).days + 1))
+
+                    second_date = result
+                    # вывод всех
+                    for d in date_range(first_date, second_date):
+                        print(d.strftime('%Y.%m.%d'))
+
+
     start(message)
-    print('1')
+
+
 # Запрашиваем URL
-#page = requests.get('https://www.hltv.org/stats/players?startDate=all')
+# page = requests.get('https://www.hltv.org/stats/players?startDate=all')
 
 # Скачиваем страницу
-#soup = BeautifulSoup(page.content, "html.parser")
+# soup = BeautifulSoup(page.content, "html.parser")
 
 # Скрапим данные
 
-#for i in range(1, 8):
+# for i in range(1, 8):
 #    name = soup.select('tr')[i].select('a')[0]['href']
 #    i += 1
 #
 #    name = name.replace('?startDate=all', '').replace('/stats/players/', '')
 #    player_list.append(name)
-    # print(name)
+# print(name)
 count_lines = sum(1 for line in open('players.txt', 'r'))  # волшебный подсчет строк в файле
-
 
 # Формирование списка из ников и айди
 for i in range(0, count_lines):
     with open('players.txt', 'r') as f:
         player_list.append(f.readlines()[i].replace('\n', ''))
     i += 0
-
 
 print(player_list)
 
@@ -95,10 +123,10 @@ for j in range(0, len(player_list)):
         print("NO")
         j += 1
 
-print('https://www.hltv.org/stats/players/matches/'+player_link)
+print('https://www.hltv.org/stats/players/matches/' + player_link)
 
 # Запрашиваем URL
-page = requests.get('https://www.hltv.org/stats/players/matches/'+player_link)
+page = requests.get('https://www.hltv.org/stats/players/matches/' + player_link)
 
 # Скачиваем страницу
 soup = BeautifulSoup(page.content, "html.parser")
@@ -117,10 +145,6 @@ else:
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, "Hello world")
-
-
-
-
 
 
 if __name__ == '__main__':
