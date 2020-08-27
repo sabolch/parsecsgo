249de001834538f25ed4184d6ad2b1dfdf9e0d81
Залим Лампежев <sabolch@yandex.ru>
2020-08-27 07:10:51 +0300

    Add calendar

--- main.py
+++ main.py
@@ -2,17 +2,45 @@ import telebot
 import requests
 from datetime import datetime, date, time
 from bs4 import BeautifulSoup
+from telegram_bot_calendar import DetailedTelegramCalendar, LSTEP
 
 bot = telebot.TeleBot('1089262513:AAEKgwH1drH3vymrnaN79h1jM8JEQ4HRpK0')
 player_link = ''
 player_list = []
-d_user = input("date")
 
-dateString = d_user
-dateFormatter = "%d.%m.%Y"
-print(datetime.strptime(dateString, dateFormatter))
+# d_user = input("date")
+
+d1 = date(2020, 8, 1)
+d2 = date.today()
+print(d2)
 
 
+def start(m):
+    calendar, step = DetailedTelegramCalendar().build()
+    bot.send_message(m.chat.id,
+                     f"Select {LSTEP[step]}",
+                     reply_markup=calendar)
+
+
+@bot.callback_query_handler(func=DetailedTelegramCalendar.func())
+def cal(c):
+    result, key, step = DetailedTelegramCalendar(locale='ru', min_date=d1, max_date=d2).process(c.data)
+    if not result and key:
+        bot.edit_message_text(f"Select {LSTEP[step]}",
+                              c.message.chat.id,
+                              c.message.message_id,
+                              reply_markup=key)
+    elif result:
+        bot.edit_message_text(f"Min: {result}",
+                              c.message.chat.id,
+                              c.message.message_id)
+
+
+# Декоратор для обработки всех текстовых сообщений
+@bot.message_handler(content_types=['text'])
+def all_messages(message):
+    start(message)
+    print('1')
 # Запрашиваем URL
 #page = requests.get('https://www.hltv.org/stats/players?startDate=all')
 
@@ -78,11 +106,7 @@ def start_message(message):
 
 
 
-# Декоратор для обработки всех текстовых сообщений
-@bot.message_handler(content_types=['text'])
-def all_messages(message):
 
-    print('1')
 
 if __name__ == '__main__':
     bot.polling(none_stop=True, timeout=123)
